3d6d7423eeaefa170701bc4d46ea990f77482e2e
Hannelize Human <hannelize.human@gmail.com>
2021-03-17 13:05:41 +1100

    Variables and variable decleration added. Prompt messages created

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
+/* Password Generator will be able to provide a passwprd of between 8-128 characters, including/excluding
+a variation of characters depending on what the user specifies*/
+
+
+
+// Password variables:
+// Special Characters:
+spcharacter = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "'", "\;", "\:", ".", "<", ">", "?", "/", "[", "]", "{", "}", "\\", "~"];
+//Numeric Characters:
+number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+// Alphabet Characters
+alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+// User input variables:
+var enterLength = "";
+var confirmNumber;
+var confirmSpCharacter;
+var confirmUppercase;
+var confirmLowercase;
+
+//Prompt to confirm how long user would like the password:
+function generatePassword() {
+  var enterLength = (prompt("How many characters would you like your password to contain? Choose a number between 8 and 128."));
+  if (!enterLength) {
+      alert("Please enter a value");
+  } else if (enterLength <= 8 || enterLength >= 128) {
+      enterLength = parseInt(prompt("You must choose an amount between 8 and 128"));
+
+  } else {
+    // Continues once user entered a valid input
+    confirmNumber = confirm("Do you want to include numbers?");
+    confirmSpCharacter = confirm("Do you want to include special characters?");
+    confirmUppercase = confirm("Do you want to include uppercase characters?");
+    confirmLowercase = confirm("Do you want to include lowercase characters?");
+  };
+
+
+  
+
+}
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
